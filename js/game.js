7d1c93c39ae3b98b581cef62f6beb1d177602324
Lidia A <lidia.lasy@gmail.com>
2022-12-08 20:16:27 +0100

    Creación archivos. str, style y funccion

---
+++ js/game.js
@@ -0,0 +1,33 @@
+function plant(seed, water, fert, temp) {
+    let myplant = [];
+    for (let g = 0; g < water; g += 1) {
+        for (let w = 0; w < water; w += 1) {
+            myplant.push('-');
+        }
+
+        for (let f = 0; f < fert; f += 1) {
+            myplant.push(seed);
+        }
+    }
+
+    if (temp >= 20 && temp <= 30) {
+        return myplant.join('');
+    } else {
+        let deadPlant = myplant.filter(tail => tail==='-');
+        deadPlant.push(seed);
+        return deadPlant.join('');
+    }
+}
+
+function createPlant () {
+    const plantPlace = document.getElementById('myplant');
+
+    let seedValue = document.getElementById('seed').value;
+    let waterValue = document.getElementById('water').value;
+    let fertValue = document.getElementById('fert').value;
+    let tempValue = document.getElementById('temp').value;
+
+    const loadPlant = plant(seedValue, waterValue, fertValue, tempValue);
+
+    plantPlace.innerHTML = loadPlant;
+}
